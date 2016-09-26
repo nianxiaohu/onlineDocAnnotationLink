443ed184c09717bb72a31d3b54b97bb4f445c8fd
Xiaohunian <xiaohunian@gmail.com>
2016-09-26 08:37:11 -0700

    use git add -A . to add all current directory files

---
+++ app/controllers/articles_controller.rb
@@ -0,0 +1,42 @@
+class ArticlesController < ApplicationController
+  http_basic_authenticate_with name:"xiaohu", password:"dan", except: [:index, :show]
+  def show
+    @article = Article.find(params[:id])
+  end
+def edit
+  @article = Article.find(params[:id])
+end
+def new
+  @article = Article.new
+end
+def index
+  @articles = Article.all
+end
+def create
+  @article = Article.new(params[:article].permit(:title, :text))
+  if @article.save
+    redirect_to @article
+  else
+    render 'new'
+   end
+end
+def update
+ @article=Article.find(params[:id])
+ if @article.update(article_params)
+   redirect_to @article
+ else
+   render 'edit'
+  end
+end
+def destroy
+  @article = Article.find(params[:id])
+  @article.destroy
+  redirect_to articles_path
+end
+
+private 
+ def article_params
+   params.require(:article).permit(:title,:text)
+ end
+end
+
